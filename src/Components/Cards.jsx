f13e9a72c25090e7b777a0ae94f4367a1d7ae13c
insharahkalam <insharahkalam19@gmail.com>
2026-02-13 19:38:54 +0500

    pagination done

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -3,8 +3,8 @@ import { GoArrowUpRight } from "react-icons/go";
 
 const Cards = () => {
     const [countries, setCountries] = useState([])
-    const [currentPage, setCurrentPage] = useState(1); // kaunsa page show ho raha
-    const itemsPerPage = 42;
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 60;
 
     useEffect(() => {
         fetch("https://restcountries.com/v3.1/independent?status=true")
@@ -14,14 +14,27 @@ const Cards = () => {
             })
     }, [])
 
-    const indexOfLastItem = currentPage * itemsPerPage;  // last item ka index
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage; // first item ka index
+    const indexOfLastItem = currentPage * itemsPerPage;
+    console.log(indexOfLastItem);
+
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    console.log(indexOfFirstItem);
+    console.log(countries);
+
+
     const currentCountries = countries.slice(indexOfFirstItem, indexOfLastItem);
+    console.log(currentCountries);
+
 
     const totalPages = Math.ceil(countries.length / itemsPerPage);
+    console.log(totalPages);
+
     const pageNumbers = [];
     for (let i = 1; i <= totalPages; i++) {
         pageNumbers.push(i);
+        console.log(i);
+        console.log(pageNumbers);
+
     }
 
     return (
@@ -33,7 +46,7 @@ const Cards = () => {
                         <img className='rounded-2xl h-[190px] w-[350px]' src={one.flags.png} alt="" />
                         <div className='my-2 border-t w-full border-gray-200'>
                             <p className=" text-3xl line-clamp-1 text-center font-bold font-valty text-body">{one.name.common}</p>
-                            <button class="flex font-normal w-full text-center items-center gap-1 text-sm font-serif capitalize justify-center hover:underline">
+                            <button className="flex font-normal w-full text-center items-center gap-1 text-sm font-serif capitalize justify-center hover:underline">
                                 View Details <GoArrowUpRight />
                             </button>
                         </div>
@@ -41,63 +54,44 @@ const Cards = () => {
 
                 ))}
 
-                {/* <div className='flex items-end justify-end'>
-
-                    <div className="pagination">
-                        <button className='border px-3 py-1 rounded-s-sm font-bold text-sm' onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}>Prev</button>
-
-                        {pageNumbers.map((number) => (
-                            <button
-                                key={number}
-                                onClick={() => setCurrentPage(number)}
-                                className={currentPage === number ? "text-red-700" : 'border px-2 py-1 font-bold text-sm'}
-                            >
-                                {number}
-                            </button>
-                        ))}
-
-                        <button className='border px-3 py-1 rounded-e-sm font-bold text-sm' onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}>Next</button>
-                    </div>
-
-                </div> */}
-
-                <div className='flex items-end justify-end w-full' >
-
-                    <button
-                        className='px-4 py-2 font-bold text-sm hover:bg-gray-200 transition'
-                        onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
-                    >
-                        Prev
-                    </button>
-
-                    {pageNumbers.map((number) => (
-                        <button
-                            key={number}
-                            onClick={() => setCurrentPage(number)}
-                            className={`px-3 py-2 text-sm font-bold transition ${currentPage === number
-                                ? 'bg-red-500 text-white'
-                                : 'bg-white text-gray-700 hover:bg-gray-200'
-                                }`}
-                        >
-                            {number}
-                        </button>
-                    ))}
-
-                    <button
-                        className='px-4 py-2 font-bold text-sm hover:bg-gray-200 transition'
-                        onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
-                    >
-                        Next
-                    </button>
-
-                </div>
-
 
             </section>
+            <div className='my-10 px-6 flex items-end justify-center w-full' >
+
+                <button
+                    className={`px-4 py-2 font-bold rounded-full border text-gray-700 border-gray-200 text-sm hover:bg-gray-200 transition ${currentPage === 1 ? 'cursor-not-allowed' : 'cursor-pointer'}`}
+                    onClick={() => currentPage > 1 && setCurrentPage(currentPage - 1)
+                    }
+                >
+                    Prev
+                </button>
+
+                {pageNumbers.map((number) => (
+                    <button
+                        key={number}
+                        onClick={() => setCurrentPage(number)}
+                        className={`py-2 px-3.5 text-sm font-bold rounded-full border border-gray-200 transition ${currentPage === number
+                            ? 'bg-gray-500 text-white'
+                            : 'bg-white text-gray-700 hover:bg-gray-200'
+                            }`}
+                    >
+                        {number}
+                    </button>
+                ))}
+
+                <button
+                    className={`px-4 py-2 font-bold rounded-full border text-gray-700 border-gray-200 text-sm hover:bg-gray-200 transition ${currentPage === 4 ? 'cursor-not-allowed' : 'cursor-pointer'}`}
+                    onClick={() => currentPage < totalPages && setCurrentPage(currentPage + 1)}
+                >
+                    Next
+                </button>
+
+            </div >
         </>
     )
 }
 
 export default Cards
+
 
 
